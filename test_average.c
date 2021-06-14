196244ec9bd313e2d133c274c3089390e4881991
Akhilappu123 <84575611+Akhilappu123@users.noreply.github.com>
2021-06-14 11:57:48 +0530

    Update test_average.c

--- test_average.c
+++ test_average.c
@@ -12,3 +12,8 @@ int main(void) {
   assert (average(y, 1) == 5.0);
   printf("Average: passed\n");
   }
+float average(int numbers[],int num) {
+int ret=0;
+for (int i=0;i<num;i++) {
+   ret+=numbers[i];}
+return ret/num; }
